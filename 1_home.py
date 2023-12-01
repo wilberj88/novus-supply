a10caf9a51e2928fb228bd6b3661cf62fce701bc
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-12-01 16:33:22 +0100

    Update 1_home.py

--- 1_home.py
+++ 1_home.py
@@ -1,4 +1,17 @@
 import streamlit as st
+from streamlit_card import card
+from streamlit_extras.grid import grid
+from streamlit_extras.colored_header import colored_header
+import folium
+from streamlit_folium import st_folium
+import time
+from streamlit_echarts import st_echarts
+import pytrends
+from pytrends.request import TrendReq
+import requests
+import pandas as pd
+from streamlit_extras.let_it_rain import rain 
+import plotly.graph_objects as go
 
 
 st.set_page_config(
