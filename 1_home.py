a69bc61c988ec6191b7c3a608b95e8810bc5689a
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-12-01 16:05:20 +0100

    Update 1_home.py

--- 1_home.py
+++ 1_home.py
@@ -13,7 +13,7 @@ st.write("""
 
 st.sidebar.markdown("Desarrollado por Novus Mando, S.L. (www.novussolutions.io)")
 
-a = st.selectbox("Selecciona un Mando 🎮 🚚 Supply Chain 🏭", ("Demanda", "Inventario", "Producción", "Almacenamiento", "Distribución", "Comercialización", "Ventas", "PostVentas", "Finanzas", "Impacto"), index=None, placeholder="Choose an option")
+a = st.selectbox("Selecciona un Mando 🎮 de la Cadena de Suministro:", ("Demanda", "Inventario", "Producción", "Almacenamiento", "Distribución", "Comercialización", "Ventas", "PostVentas", "Finanzas", "Impacto"), index=None, placeholder="Choose an option")
 
 if a:
-  b = st.selectbox("Selecciona el módulo de análisis 🔎", ("Histórico y 2024", "Estrategia", "Alarmas", "Recomendaciones"), index=None, placeholder="Choose an option")
+  b = st.selectbox("Selecciona el módulo de análisis 🔎:", ("Histórico y 2024", "Estrategia", "Alarmas", "Recomendaciones"), index=None, placeholder="Choose an option")
