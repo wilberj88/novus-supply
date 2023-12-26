2e534be651ce6207630682b2697430fa54f8e1ca
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-12-27 00:05:45 +0100

    Update 1_home.py

--- 1_home.py
+++ 1_home.py
@@ -102,12 +102,11 @@ with colC:
   lottie_json = load_lottiefile(lottie_file)
   st_lottie(lottie_json, height=200)
 
-
-container = st.container(border=True)
-container.write("Monitoreo en Tiempo Real")
-lottie_file3 = 'data/realtime.json'
-lottie_json3 = load_lottiefile(lottie_file3)
-container.st_lottie(lottie_json3, height=300)
+with st.container():
+  st.header('Monitoreo Mundial en Tiempo Real')
+  lottie_file3 = 'data/realtime.json'
+  lottie_json3 = load_lottiefile(lottie_file3)
+  container.st_lottie(lottie_json3, height=300)
 
 
 colX, colY = st.columns(2)
